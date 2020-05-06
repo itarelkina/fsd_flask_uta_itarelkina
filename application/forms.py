23f63e2c1f32ce1d5b500649e913398a8f82c1e3
Irina Tarelkina <itarelkina@yahoo.ca>
2020-05-06 14:29:05 -0400

    finishes with forms flask and security

--- application/forms.py
+++ application/forms.py
@@ -10,7 +10,7 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Login")
 
 class RegisterForm(FlaskForm):
-    email   = StringField("Email", validators=[DataRequired()])
+    email   = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired(),Length(min=6,max=15)])
     password_confirm = PasswordField("Confirm Password", validators=[DataRequired(),Length(min=6,max=15), EqualTo('password')])
     first_name = StringField("First Name", validators=[DataRequired(),Length(min=2,max=55)])
